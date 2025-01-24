778932f6e5d115f0dbdb207b46db052cea0e3e78
Aqsa github <aqsas2774@gmail.com>
2025-01-24 19:51:31 +0500

    Added some files

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,8 @@ import express from "express";
 const router = express.Router();
 import { authenticate, authorizeAdmin } from '../middlewares/auth.js';
 import { userLogin, logoutUser, addUser } from "../controllers/userControllers.js";
-router.route('/login').post(authenticate, userLogin);
+router.route('/login').post(userLogin);
 router.route('/logout').post(authenticate, logoutUser);
 router.route('/add').post(addUser);
-
 
 export default router;
